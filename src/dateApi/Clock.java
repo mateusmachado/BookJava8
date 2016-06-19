d258151a62ffae3b126ac4c0a9a71e8d93e154a9
Mateus <mmachado.csp@gmail.com>
2016-06-19 01:45:37 -0300

    example dateApi

---
+++ src/dateApi/Clock.java
@@ -0,0 +1,17 @@
+package dateApi;
+
+import java.time.ZoneId;
+
+public class Clock {
+	public static void main(String[] args) {
+
+		System.out.println(ZoneId.getAvailableZoneIds());
+
+		ZoneId zone1 = ZoneId.of("Europe/Berlin");
+		ZoneId zone2 = ZoneId.of("Brazil/East");
+		System.out.println(zone1.getRules());
+		System.out.println(zone2.getRules());
+
+	}
+
+}
