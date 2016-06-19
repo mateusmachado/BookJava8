2b01185a47f90305576935f8bddec87416fc8127
Mateus <mmachado.csp@gmail.com>
2016-06-19 00:44:15 -0300

    validate CEP

---
+++ src/functionalInterface/MainValidate.java
@@ -0,0 +1,10 @@
+package functionalInterface;
+
+public abstract class MainValidate<T> {
+	public static void main(String[] args) {
+
+		Validator<String> validateCEP = v -> v.matches("[0-9]{5}-[0-9]{3}");
+		System.out.println(validateCEP.validate("1234-567"));
+		
+	}
+}
---
+++ src/functionalInterface/Validator.java
@@ -0,0 +1,5 @@
+package functionalInterface;
+
+public interface Validator<T> {
+	boolean validate(T t);
+}
